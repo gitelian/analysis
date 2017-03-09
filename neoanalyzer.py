c52a2894362a5c36fa10e74bda8c7adfabc7ce57
gitelian <gitelian@berkeley.edu>
2017-03-09 09:53:21 -0800

    multipage PDF creation for unit summaries

--- neoanalyzer.py
+++ neoanalyzer.py
@@ -1233,102 +1233,120 @@ plt.show()
 
 neuro.get_burst_isi()
 
-# for unit_index in num_units:
-unit_index = 11
+from matplotlib.backends.backend_pdf import PdfPages
+with PdfPages(fid + '_unit_summaries.pdf') as pdf:
+    for unit_index in range(neuro.num_units):
 
-# get best contact position from evoked rates
-meanr     = np.array([np.mean(k[:, unit_index]) for k in neuro.evk_rate])
-meanr_abs = np.array([np.mean(k[:, unit_index]) for k in neuro.abs_rate])
-best_contact = np.argmax(meanr[0:8])
+        # get best contact position from evoked rates
+        meanr     = np.array([np.mean(k[:, unit_index]) for k in neuro.evk_rate])
+        meanr_abs = np.array([np.mean(k[:, unit_index]) for k in neuro.abs_rate])
+        best_contact = np.argmax(meanr[0:8])
 
-fig, ax = plt.subplots(3, 3)
-fig.suptitle('Region: {}, depth: {}, unit type: {}, mouse: {}'.format(\
-        neuro.region_dict[neuro.shank_ids[unit_index]], \
-        neuro.neo_obj.segments[0].spiketrains[unit_index].annotations['depth'], \
-        neuro.cell_type[unit_index], \
-        fid))
+        fig, ax = plt.subplots(3, 3, figsize=(10,8))
+        fig.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.32, hspace=0.45)
+        fig.suptitle('Region: {}, depth: {}, unit type: {}, mouse: {}'.format(\
+                neuro.region_dict[neuro.shank_ids[unit_index]], \
+                neuro.neo_obj.segments[0].spiketrains[unit_index].annotations['depth'], \
+                neuro.cell_type[unit_index], \
+                fid))
 
-# top left: best contact PSTH
-neuro.plot_psth(axis=ax[0][0], unit_ind=unit_index, trial_type=best_contact, error='sem', color='k')
-neuro.plot_psth(axis=ax[0][0], unit_ind=unit_index, trial_type=best_contact+9, error='sem', color='r')
-neuro.plot_psth(axis=ax[0][0], unit_ind=unit_index, trial_type=best_contact+9+9, error='sem', color='b')
-ax[0][0].set_xlim(0, 2)
-ax[0][0].hlines(0, 0, 2, colors='k', linestyles='dashed')
-ax[0][0].set_xlabel('time (s)')
-ax[0][0].set_ylabel('firing rate (Hz)')
+        # top left: best contact PSTH
+        neuro.plot_psth(axis=ax[0][0], unit_ind=unit_index, trial_type=best_contact, error='sem', color='k')
+        neuro.plot_psth(axis=ax[0][0], unit_ind=unit_index, trial_type=best_contact+9, error='sem', color='r')
+        neuro.plot_psth(axis=ax[0][0], unit_ind=unit_index, trial_type=best_contact+9+9, error='sem', color='b')
+        ax[0][0].set_xlim(0, 2)
+        ax[0][0].hlines(0, 0, 2, colors='k', linestyles='dashed')
+        ax[0][0].vlines(0.5, ax[0][0].get_ylim()[0], ax[0][0].get_ylim()[1], colors='m', linestyles='dashed')
+        ax[0][0].vlines(1.5, ax[0][0].get_ylim()[0], ax[0][0].get_ylim()[1], colors='m', linestyles='dashed')
+        ax[0][0].set_xlabel('time (s)')
+        ax[0][0].set_ylabel('firing rate (Hz)')
+        ax[0][0].set_title('best contact')
 
-# top middle: control PSTH
-neuro.plot_psth(axis=ax[0][1], unit_ind=unit_index, trial_type=neuro.control_pos-1, error='sem', color='k')
-neuro.plot_psth(axis=ax[0][1], unit_ind=unit_index, trial_type=neuro.control_pos-1+9, error='sem', color='r')
-neuro.plot_psth(axis=ax[0][1], unit_ind=unit_index, trial_type=neuro.control_pos-1+9+9, error='sem', color='b')
-ax[0][1].set_xlim(0, 2)
-ax[0][1].hlines(0, 0, 2, colors='k', linestyles='dashed')
-ax[0][1].set_xlabel('time (s)')
+        # top middle: control PSTH
+        neuro.plot_psth(axis=ax[0][1], unit_ind=unit_index, trial_type=neuro.control_pos-1, error='sem', color='k')
+        neuro.plot_psth(axis=ax[0][1], unit_ind=unit_index, trial_type=neuro.control_pos-1+9, error='sem', color='r')
+        neuro.plot_psth(axis=ax[0][1], unit_ind=unit_index, trial_type=neuro.control_pos-1+9+9, error='sem', color='b')
+        ax[0][1].set_xlim(0, 2)
+        ax[0][1].vlines(0.5, ax[0][1].get_ylim()[0], ax[0][1].get_ylim()[1], colors='m', linestyles='dashed')
+        ax[0][1].vlines(1.5, ax[0][1].get_ylim()[0], ax[0][1].get_ylim()[1], colors='m', linestyles='dashed')
+        ax[0][1].hlines(0, 0, 2, colors='k', linestyles='dashed')
+        ax[0][1].set_xlabel('time (s)')
+        ax[0][1].set_title('no contact')
 
-# top right: evoked tuning curves
-neuro.plot_tuning_curve(unit_ind=unit_index, kind='evk_count', axis=ax[0][2])
-ax[0][2].set_xlim(0, 10)
-ax[0][2].hlines(0, 0, 10, colors='k', linestyles='dashed')
-ax[0][2].set_xlabel('bar position')
+        # top right: evoked tuning curves
+        neuro.plot_tuning_curve(unit_ind=unit_index, kind='evk_count', axis=ax[0][2])
+        ax[0][2].set_xlim(0, 10)
+        ax[0][2].hlines(0, 0, 10, colors='k', linestyles='dashed')
+        ax[0][2].set_xlabel('bar position')
+        ax[0][2].set_title('evoked tc')
 
-# middle left: ISI distributions best contact
-bins = np.arange(0, 0.100, 0.001);
-a, _=np.histogram(neuro.isi_list[best_contact][unit_index][:,0], bins=bins, density=True)
-b, _=np.histogram(neuro.isi_list[best_contact+9][unit_index][:,0], bins=bins, density=True)
-c, _=np.histogram(neuro.isi_list[best_contact+9+9][unit_index][:,0], bins=bins, density=True)
-ax[1][0].plot(bins[:-1], a, 'k', bins[:-1], b, 'r', bins[:-1], c, 'b')
-ax[1][0].set_xlim(-0.005, 0.100)
-ax[1][0].vlines(0.0015, ax[1][0].get_ylim()[0], ax[1][0].get_ylim()[1], colors='k', linestyles='dashed')
-ax[1][0].set_xlabel('ISI (s)')
+        # middle left: ISI distributions best contact
+        bins = np.arange(0, 0.100, 0.001);
+        a, _=np.histogram(neuro.isi_list[best_contact][unit_index][:,0], bins=bins, density=True)
+        b, _=np.histogram(neuro.isi_list[best_contact+9][unit_index][:,0], bins=bins, density=True)
+        c, _=np.histogram(neuro.isi_list[best_contact+9+9][unit_index][:,0], bins=bins, density=True)
+        ax[1][0].plot(bins[:-1], a, 'k', bins[:-1], b, 'r', bins[:-1], c, 'b')
+        ax[1][0].set_xlim(-0.005, 0.100)
+        ax[1][0].vlines(0.0015, ax[1][0].get_ylim()[0], ax[1][0].get_ylim()[1], colors='k', linestyles='dashed')
+        ax[1][0].set_xlabel('ISI (s)')
+        ax[1][0].set_title('ISI distribution')
 
-# middle middle: ISI distributions control position
-bins = np.arange(0, 0.100, 0.001);
-aa, _=np.histogram(neuro.isi_list[neuro.control_pos-1][unit_index][:,0], bins=bins, density=True)
-bb, _=np.histogram(neuro.isi_list[neuro.control_pos-1+9][unit_index][:,0], bins=bins, density=True)
-cc, _=np.histogram(neuro.isi_list[neuro.control_pos-1+9+9][unit_index][:,0], bins=bins, density=True)
-ax[1][1].plot(bins[:-1], aa, 'k', bins[:-1], bb, 'r', bins[:-1], cc, 'b')
-ax[1][1].set_xlim(-0.005, 0.100)
-ax[1][1].vlines(0.0015, ax[1][1].get_ylim()[0], ax[1][1].get_ylim()[1], colors='k', linestyles='dashed')
-ax[1][1].set_xlabel('ISI (s)')
+        # middle middle: ISI distributions control position
+        bins = np.arange(0, 0.100, 0.001);
+        aa, _=np.histogram(neuro.isi_list[neuro.control_pos-1][unit_index][:,0], bins=bins, density=True)
+        bb, _=np.histogram(neuro.isi_list[neuro.control_pos-1+9][unit_index][:,0], bins=bins, density=True)
+        cc, _=np.histogram(neuro.isi_list[neuro.control_pos-1+9+9][unit_index][:,0], bins=bins, density=True)
+        ax[1][1].plot(bins[:-1], aa, 'k', bins[:-1], bb, 'r', bins[:-1], cc, 'b')
+        ax[1][1].set_xlim(-0.005, 0.100)
+        ax[1][1].vlines(0.0015, ax[1][1].get_ylim()[0], ax[1][1].get_ylim()[1], colors='k', linestyles='dashed')
+        ax[1][1].set_xlabel('ISI (s)')
+        ax[1][1].set_title('ISI distribution')
 
-# middle right: OMI tuning curves
-omi_s1light = (meanr_abs[neuro.control_pos:neuro.control_pos+9] - meanr_abs[:neuro.control_pos]) / \
-        (meanr_abs[neuro.control_pos:neuro.control_pos+9] + meanr_abs[:neuro.control_pos])
-omi_m1light = (meanr_abs[neuro.control_pos+9:neuro.control_pos+9+9] - meanr_abs[:neuro.control_pos]) / \
-        (meanr_abs[neuro.control_pos+9:neuro.control_pos+9+9] + meanr_abs[:neuro.control_pos])
-ax[1][2].plot(np.arange(1,10), omi_s1light, '-ro', np.arange(1,10), omi_m1light, '-bo')
-ax[1][2].hlines(0, 0, 10, colors='k', linestyles='dashed')
-ax[1][2].set_xlim(0, 10)
-ax[1][2].set_ylim(-1, 1)
-ax[1][2].set_xlabel('bar position')
-ax[1][2].set_ylabel('OMI')
+        # middle right: OMI tuning curves
+        omi_s1light = (meanr_abs[neuro.control_pos:neuro.control_pos+9] - meanr_abs[:neuro.control_pos]) / \
+                (meanr_abs[neuro.control_pos:neuro.control_pos+9] + meanr_abs[:neuro.control_pos])
+        omi_m1light = (meanr_abs[neuro.control_pos+9:neuro.control_pos+9+9] - meanr_abs[:neuro.control_pos]) / \
+                (meanr_abs[neuro.control_pos+9:neuro.control_pos+9+9] + meanr_abs[:neuro.control_pos])
+        ax[1][2].plot(np.arange(1,10), omi_s1light, '-ro', np.arange(1,10), omi_m1light, '-bo')
+        ax[1][2].hlines(0, 0, 10, colors='k', linestyles='dashed')
+        ax[1][2].set_xlim(0, 10)
+        ax[1][2].set_ylim(-1, 1)
+        ax[1][2].set_xlabel('bar position')
+        ax[1][2].set_ylabel('OMI')
+        ax[1][2].set_title('OMI tc')
+
+        # bottom left: bursty ISI plot best position
+        a_pre  = neuro.bisi_list[best_contact][unit_index][:, 0]
+        a_post = neuro.bisi_list[best_contact][unit_index][:, 1]
+        #b_pre  = neuro.bisi_list[best_contact+9][unit_index][:, 0]
+        #b_post = neuro.bisi_list[best_contact+9][unit_index][:, 1]
+        #c_pre  = neuro.bisi_list[best_contact+9+9][unit_index][:, 0]
+        #c_post = neuro.bisi_list[best_contact+9+9][unit_index][:, 1]
+        ax[2][0].plot(a_pre, a_post, 'k.', alpha=0.3, ms=5)
+        #ax[2][0].plot(b_pre, b_post, 'r.', alpha=0.3, ms=5)
+        #ax[2][0].plot(c_pre, c_post, 'b.', alpha=0.3, ms=5)
+        ax[2][0].set_xlim(0, 0.100)
+        ax[2][0].set_ylim(0, 0.100)
+        ax[2][0].set_xlabel('pre ISI (s)')
+        ax[2][0].set_ylabel('post ISI (s)')
+        ax[2][0].set_title('Burst ISI distribution')
+
+        # bottom middle: bursty ISI plot control position
+        aa_pre  = neuro.bisi_list[neuro.control_pos-1][unit_index][:, 0]
+        aa_post = neuro.bisi_list[neuro.control_pos-1][unit_index][:, 1]
+        ax[2][1].plot(aa_pre, aa_post, 'k.', alpha=0.3, ms=5)
+        ax[2][1].set_xlim(0, 0.100)
+        ax[2][1].set_ylim(0, 0.100)
+        ax[2][1].set_xlabel('pre ISI (s)')
+        ax[2][1].set_ylabel('post ISI (s)')
+        ax[2][1].set_title('Burst ISI distribution')
+
+        #ax[1][0].hist2d(pre, post, bins=arange(0,0.3,0.001))
 
 
-# bottom left: bursty ISI plot best position
-a_pre  = neuro.bisi_list[best_contact][unit_index][:, 0]
-a_post = neuro.bisi_list[best_contact][unit_index][:, 1]
-#b_pre  = neuro.bisi_list[best_contact+9][unit_index][:, 0]
-#b_post = neuro.bisi_list[best_contact+9][unit_index][:, 1]
-#c_pre  = neuro.bisi_list[best_contact+9+9][unit_index][:, 0]
-#c_post = neuro.bisi_list[best_contact+9+9][unit_index][:, 1]
-ax[2][0].plot(a_pre, a_post, 'k.', alpha=0.3, ms=5)
-#ax[2][0].plot(b_pre, b_post, 'r.', alpha=0.3, ms=5)
-#ax[2][0].plot(c_pre, c_post, 'b.', alpha=0.3, ms=5)
-ax[2][0].set_xlim(0, 0.100)
-ax[2][0].set_ylim(0, 0.100)
-ax[2][0].set_xlabel('pre ISI (s)')
-ax[2][0].set_ylabel('post ISI (s)')
-
-# bottom middle: bursty ISI plot control position
-aa_pre  = neuro.bisi_list[neuro.control_pos-1][unit_index][:, 0]
-aa_post = neuro.bisi_list[neuro.control_pos-1][unit_index][:, 1]
-ax[2][1].plot(aa_pre, aa_post, 'k.', alpha=0.3, ms=5)
-ax[2][1].set_xlim(0, 0.100)
-ax[2][1].set_ylim(0, 0.100)
-ax[2][1].set_xlabel('pre ISI (s)')
-ax[2][1].set_ylabel('post ISI (s)')
-
-#ax[1][0].hist2d(pre, post, bins=arange(0,0.3,0.001))
+        pdf.savefig()
+        fig.clear()
+        plt.close()
 
 
 
