abd1d115e704486c40f293a8d3d59f3fe07baa71
gitelian <gitelian@berkeley.edu>
2018-10-26 14:02:22 -0700

    user must accept or reject fits

--- fit_tuning.py
+++ fit_tuning.py
@@ -29,6 +29,11 @@ m1_fs = list()
 s1_rs = list()
 s1_fs = list()
 
+m1_rs_sel = list()
+m1_fs_sel = list()
+s1_rs_sel = list()
+s1_fs_sel = list()
+
 fids = ['1336', '1338', '1339', '1340', '1343', '1345']
 experiments = list()
 for fid_name in fids:
@@ -42,42 +47,94 @@ for neuro in experiments:
             meanr_abs = np.array([np.mean(k[:, unit_index]) for k in neuro.abs_rate])
             best_contact = np.argmax(meanr_abs[0:8])
 
-            if best_contact > 0 and best_contact < 7:
-                y = meanr_abs[0:8]
-                y = y/np.max(y)
+#            if best_contact > 0 and best_contact < 7:
+            y = meanr_abs[0:8]
+            y = y/np.max(y)
 
-                gmodel = Model(gaussian)
-                result = gmodel.fit(y, x=x, amp=1, cen=best_contact, wid=1)
+            gmodel = Model(gaussian)
+            result = gmodel.fit(y, x=x, amp=1, cen=best_contact, wid=1)
 
-    #            print(result.fit_report())
+#            print(result.fit_report())
 
-                if result.chisqr < 0.13:
+            #if result.chisqr < 0.13:
+            plt.figure()
+            plt.plot(x, y, 'bo--', linewidth=0.5)
+        #    plt.plot(x, result.init_fit, 'k--')
+            plt.plot(x, result.best_fit, 'r-')
+            plt.title('fwhm: {} | chisqr: {} | region: {}'.format(fwhm(result), result.chisqr, neuro.shank_ids[unit_index]))
+            plt.show()
+            accept = raw_input('accept fit?')
+            plt.close()
 
-                    if neuro.shank_ids[unit_index] == 0:
+            if accept == 'y':
 
-                        if neuro.cell_type[unit_index] == 'RS':
-                            m1_rs.append(fwhm(result))
+                if neuro.shank_ids[unit_index] == 0:
 
-                        elif neuro.cell_type[unit_index] == 'FS':
-                            m1_fs.append(fwhm(result))
-                    elif neuro.shank_ids[unit_index] == 1:
+                    if neuro.cell_type[unit_index] == 'RS':
+                        m1_rs.append(fwhm(result))
+                        m1_rs_sel.append(neuro.selectivity[unit_index, 0])
 
-                        if neuro.cell_type[unit_index] == 'RS':
-                            s1_rs.append(fwhm(result))
+                    elif neuro.cell_type[unit_index] == 'FS':
+                        m1_fs.append(fwhm(result))
+                        m1_fs_sel.append(neuro.selectivity[unit_index, 0])
+                elif neuro.shank_ids[unit_index] == 1:
 
-                        elif neuro.cell_type[unit_index] == 'FS':
-                            s1_fs.append(fwhm(result))
+                    if neuro.cell_type[unit_index] == 'RS':
+                        s1_rs.append(fwhm(result))
+                        s1_rs_sel.append(neuro.selectivity[unit_index, 0])
+
+                    elif neuro.cell_type[unit_index] == 'FS':
+                        s1_fs.append(fwhm(result))
+                        s1_fs_sel.append(neuro.selectivity[unit_index, 0])
+
+import scipy.io as sio
+sio.savemat('/home/greg/Desktop/tuning_width.mat',
+        {'m1_rs':m1_rs,
+         'm1_fs':m1_fs,
+         's1_rs':s1_rs,
+         's1_fs':s1_fs,
+         'm1_rs_sel':m1_rs_sel,
+         'm1_fs_sel':m1_fs_sel,
+         's1_rs_sel':s1_rs_sel,
+         's1_fs_sel':s1_fs_sel,
+         })
 
 
-bins = np.arange(0, 100)
+bins = np.arange(0, 25)
+bins_sel = np.arange(0, 1, 0.05)
 
-hist(m1_rs, bins=bins, color='k', alpha=0.5)
-hist(s1_rs, bins=bins, color='r', alpha=0.5)
+fig, ax = plt.subplots(2,2)
+ax[0][0].hist(m1_rs, bins=bins, color='k', alpha=0.5)
+ax[0][0].hist(s1_rs, bins=bins, color='r', alpha=0.5)
+ax[0][0].legend(['M1', 'S1'])
+
+ax[1][0].hist(m1_rs_sel, bins=bins_sel, color='k', alpha=0.5)
+ax[1][0].hist(s1_rs_sel, bins=bins_sel, color='r', alpha=0.5)
+
+ax[0][1].hist(m1_fs, bins=bins, color='k', alpha=0.5)
+ax[0][1].hist(s1_fs, bins=bins, color='r', alpha=0.5)
+
+ax[1][1].hist(m1_fs_sel, bins=bins_sel, color='k', alpha=0.5)
+ax[1][1].hist(s1_fs_sel, bins=bins_sel, color='r', alpha=0.5)
+
+ax[0][0].set_title('FWHM RS units')
+ax[0][1].set_title('FWHM FS units')
+ax[1][0].set_title('Selectivity RS units')
+ax[1][1].set_title('Selectivity FS units')
 
 
+all_fwhm = m1_rs + s1_rs + m1_fs + s1_fs
+all_sel  = m1_rs_sel + s1_rs_sel + m1_fs_sel + s1_fs_sel
+rho, p = sp.stats.pearsonr(all_sel, all_fwhm)
 
+fig, ax = plt.subplots()
+ax.scatter(all_sel, all_fwhm, color='k')
+ax.set_xlim(0, 1)
+ax.set_ylim(0, 20)
 
-
+ax.set_xlabel('Selectivity')
+ax.set_ylabel('FWHM')
+ax.set_title('Correlation coef: {0:0.2f}, p-value: {0:.2e}'.format(rho, p))
 
 
 
