e16a0e28ae53556bf019b2e1f1ad95d9356ab958
gitelian <gitelian@berkeley.edu>
2019-01-31 10:52:40 -0800

    plot mean setpoint or amplitude

--- behavior_analyzer.py
+++ behavior_analyzer.py
@@ -22,7 +22,7 @@ from IPython.core.debugger import Tracer
 
 
 ## plot mean set-point
-fig, ax = neuro.plot_mean_setpoint(t_window=[-1.5, 1.5], cond2plot=[0, 3, 4, 7, 8], all_trials=True)
+fig, ax = neuro.plot_mean_whisker(t_window=[-1.5, 1.5], kind='setpoint', cond2plot=[0, 3, 4, 7, 8], all_trials=True)
 fig.suptitle(fid + ' mean set-point ALL TRIALS')
 ylim = ax[0].get_ylim()
 for a in ax:
@@ -30,6 +30,16 @@ for a in ax:
     a.set_ylim(ylim)
     a.axvspan(-1, 0, alpha=0.2, color='green')
 
+## plot mean amplitude
+fig, ax = neuro.plot_mean_whisker(t_window=[-1.5, 1.5], kind='amplitude', cond2plot=[0, 3, 4, 7, 8], all_trials=True)
+fig.suptitle(fid + ' mean amplitude ALL TRIALS')
+ylim = ax[0].get_ylim()
+for a in ax:
+    a.vlines([-1, 1], ylim[0], ylim[1], colors='c')
+    a.set_ylim(ylim)
+    a.axvspan(-1, 0, alpha=0.2, color='green')
+
+##TODO plot PSD of whisker angle
 
 ## plot mean set-point with NOLIGHT for CORRECT vs INCORRECT
 sp_mean_right, sp_sem_right, num_trials_right = neuro.get_setpoint(correct=True)
@@ -62,3 +72,4 @@ for k, cond in enumerate(cond2plot):
 
 #        ax[k][manip].fill_between(self.wtt[start_ind:stop_ind], mean_sp[cond + (self.control_pos*manip)][start_ind:stop_ind] - sem_sp[cond + (self.control_pos*manip)][start_ind:stop_ind],\
 #                mean_sp[cond + (self.control_pos*manip)][start_ind:stop_ind] + sem_sp[cond + (self.control_pos*manip)][start_ind:stop_ind], facecolor=line_color[manip], alpha=0.3)
+
