3e5d90e8baed576c6d7c910dcac76b33070722b4
gitelian <gitelian@berkeley.edu>
2020-02-28 17:15:13 -0800

    key notes about behavioral variables and what happened in key experiments

---
+++ experiment_data.py
@@ -0,0 +1,25 @@
+
+
+
+
+# 2133 first experiment, 794 trials, dynamice PC%, Good->Great
+# 2139 2.1mW, 1002 trials, GREAT
+# 2144 2.1mW, DISENGAGED entire experiment
+# 2146 2.1mW, 604 trials, GREAT
+
+# 1.1mW exps
+# 2164, 65, 66 all great!
+
+experiments = {
+    'JB116_R':
+    [2133,
+    2139,
+    2144,
+    2146],
+    'GT032_CE':
+    [2164]
+    'GT027_RL':
+    [2165]
+    'GT027_RT':
+    [2164]}
+
